39f7dfe78b1735f55f44bfa581e31092ee683aa1
vfbapptest <vfbapptest@gmail.com>
2015-09-15 12:35:03 -0400

    Initial Commit

---
+++ Networking-Software/src/networkingSoftware/NetworkingClass.java
@@ -0,0 +1,10 @@
+package networkingSoftware;
+
+public class NetworkingClass {
+
+	public static void main(String[] args) {
+		System.out.println("This is the networking software project");
+
+	}
+
+}
