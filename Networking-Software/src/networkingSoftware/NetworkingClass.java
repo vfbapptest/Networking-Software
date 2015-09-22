f749407a7b1eb13ebea04406536f1acf108027f9
Verifone <vfbapptest@gmail.com>
2015-09-22 14:05:51 -0400

    Update NetworkingClass.java

--- Networking-Software/src/networkingSoftware/NetworkingClass.java
+++ Networking-Software/src/networkingSoftware/NetworkingClass.java
@@ -3,7 +3,7 @@ package networkingSoftware;
 public class NetworkingClass {
 
 	public static void main(String[] args) {
-		System.out.println("This is the networking software project that's awesome");
+		System.out.println("This is the networking software project that's awesome and great");
 
 	}
 
